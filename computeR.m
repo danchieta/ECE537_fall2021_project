a1f4a31a6f0c74e50069bfb699181ee8954e193d
David Campos <davidca102@gmail.com>
2021-11-15 20:30:59 -0500

    Tested script with more wires. Added more comments.

--- computeR.m
+++ computeR.m
@@ -1,24 +1,36 @@
 function [R, z, Ez, N] = computeR(L,xloc,dz,a)
-%UNTITLED Summary of this function goes here
+%computeR Summary of this function goes here
 %   Detailed explanation goes here
 
-N = floor(L/dz)+1;
-ntotalsegments = sum(N);
-sumN = cumsum([0 N]);
-nelements = length(L);
+if length(L)~=length(xloc)
+    error('L and xloc must have the same length.')
+end
 
+N = floor(L/dz)+1;          % Number of segments at each element
+ntotalsegments = sum(N);    % Total number of segments
+sumN = cumsum([0 N]);       % will use this to calculate z and x coordinates for each segment
+nelements = length(L);      % total number of elements
+
+% Initializing vectors of coordinates
 xv = zeros(ntotalsegments,1)*nan;
 z = zeros(ntotalsegments,1)*nan;
-Ez = zeros(ntotalsegments,1);
-Ez((sumN(2)-1)/2+1) = 1;
 
+% According to the slides
+%   Enforcing this boundary condition for the E component will yield
+%   zero at every observation point, except at the feed point to the dipole
+%   where the field is assumed to be unity (scaled).
+Ez = zeros(ntotalsegments,1);
+Ez(floor(sumN(2)/2)+1) = 1;
+
+% for each element calculate the coordinates of the segments
 for i = 2:nelements+1
-    z( sumN(i-1) + 1 : sumN(i)) = -L(i-1)/2:dz:L(i-1)/2;
+    z( sumN(i-1) + 1 : sumN(i)) = -L(i-1)/2 : dz: L(i-1)/2;
     xv(sumN(i-1) + 1 : sumN(i)) = xloc(i-1)*ones(N(i-1),1);
 end
 
+% x-axis distance between segments
 dx = xv-xv';
-dx = dx + (dx==0)*a;
+dx = dx + (dx==0)*a; % add a whenever distance is zero
 
 R = sqrt((z-z').^2 + dx.^2);
 end
--- test1.m
+++ test1.m
@@ -4,12 +4,14 @@ lambda = c/freq;
 
 L = [lambda/2,...   % length of the dipole
     lambda/4,...    % length of parasite 1
-    lambda/8];      % length of parasite 2
+    lambda/5,...    % length of parasite 2
+    lambda/5];      % length of parasite 3
 x = [0,...
     lambda/8,...
-    lambda/4];  % location of wires on the x axis
+    lambda/4,...
+    lambda/2];      % location of wires on the x axis
 dz = lambda/40;     % length of a discrete segment
-a = 0.0001;         % radius of the wires
+a = 0.005;          % radius of the wires
 
 [R, z, Ez, N] = computeR(L,x,dz,a);
 
@@ -20,20 +22,18 @@ G2 = (  3 + 3i*k*R - k^2*R.^2) / (R.^5);
 
 A = (G1 + (z-z').^2.*G2).*exp(-1i*k*R);
 
-% According to the slides
-%   Enforcing this boundary condition for the E component will yield
-%   zero at every observation point, except at the feed point to the dipole
-%   where the field is assumed to be unity (scaled).
-
 Jz = A\Ez
 
 dummyN = cumsum([0 N]);
 nfigures = length(L);
 
-
 figure(1)
+clf
 for i=2:nfigures+1
     subplot(nfigures,1,i-1)
     stem(z(dummyN(i-1) + 1 : dummyN(i)), abs(Jz(dummyN(i-1) + 1 : dummyN(i))))
 	xlim([min(z) max(z)])
+    ylim([min(abs(Jz)) max(abs(Jz))])
+    xlabel('z')
+    ylabel('|J|')
 end
